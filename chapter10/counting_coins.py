441d0a3decc6c2f0e47590d6abe0c5760323953b
blvck <blvck@mars.mshome.net>
2018-08-27 14:04:54 -0600

    complete counting algorithm

--- chapter10/counting_coins.py
+++ chapter10/counting_coins.py
@@ -18,9 +18,24 @@ cv2.imshow("Edges",edged)
 
 (_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
     cv2.CHAIN_APPROX_SIMPLE)
+
 print("I count {} coins in this image".format(len(cnts)))
 
 coins = image.copy()
 cv2.drawContours(coins, cnts, -1,(0,255,0),2)
 cv2.imshow("Coins",coins)
 cv2.waitKey(0)
+
+for (i,c) in enumerate(cnts):
+    (x ,y, w, h) = cv2.boundingRect(c)
+
+    print("Coin #{}".format(i + 1))
+    coin = image[y:y + h, x:x +w]
+    cv2.imshow("Coin", coin)
+    mask = np.zeros(image.shape[:2], dtype = "uint8")
+    ((centerX, centerY), radius) = cv2.minEnclosingCircle(c)
+    cv2.circle(mask, (int(centerX), int(centerY)),int(radius),
+        255,-1)
+    mask = mask[y:y + h, x:x +w]
+    cv2.imshow("Masked Coin", cv2.bitwise_and(coin, coin, mask = mask))
+    cv2.waitKey(0)
